6c2b9874eb31cb68436ac549fbf19782e9c0fb76
klimmonmilk <tisirada@gmail.com>
2025-11-21 17:36:09 +0700

    move A-E

--- game.ts
+++ game.ts
@@ -9,17 +9,23 @@ const moves = [];
 // Start at A (0,0) -> [row][col]
 let row = 0;
 let col = 0; 
-moves.push(board[row][col]); // A
+moves.push(board[row][col]); // A // r0 c0 [0][0]
 
 // Move Right to B
 col++;
-moves.push(board[row][col]); // B
+moves.push(board[row][col]); // B // r0 c1 [0][1]
 
 // Move Right to C
+col++;
+moves.push(board[row][col]); // C // r0 c2 [0][2]
 
 // Move Down to F
+row++;
+moves.push(board[row][col]); // F // r1 c2 [1][2]
 
 // Move left to E
+col--;
+moves.push(board[row][col]); // E // r1 c1 [1][1]
 
 console.log("Path:", moves.join(" → "));
 // Output: Path: A → B → C → F → E
