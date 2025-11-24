e1efaf2f6595faac333bd54ccd77c602b91ae43c
klimmonmilk <tisirada@gmail.com>
2025-11-24 15:25:46 +0700

    submit: game movement

--- game-fn.ts
+++ game-fn.ts
@@ -6,25 +6,58 @@ const board = [
 
 const moves = [];
 
+const rowLength = board.length;
+const colLength = board[0].length;
+
 // Start at A (0,0) -> [row][col]
 let row = 0;
 let col = 0;
 moves.push(board[row][col]); // A
 
 // Define movement functions
-function moveRight() {}
+function moveRight() {
+	if (col < colLength) {
+		col++;
+		moves.push(board[row][col]);
+	} else {
+		console.log("Invalid Move");
+	}
+}
 
-function moveLeft() {}
+function moveLeft() {
+	if (col < colLength) {
+	col--;
+	moves.push(board[row][col]);
+	} else {
+		console.log("Invalid Move");
+	}
+}
 
-function moveUp() {}
+function moveUp() {
+	if (row < rowLength) {
+	row--;
+	moves.push(board[row][col]);
+	} else {
+		console.log("Invalid Move");
+	}
+}
 
-function moveDown() {}
+function moveDown() {
+	if (row < rowLength) {
+	row++;
+	moves.push(board[row][col]);
+	} else {
+		console.log("Invalid Move");
+	}
+}
 
 // Call the movement functions
 moveRight(); // Move from A to B
 moveRight(); // Move from B to C
 moveDown(); // Move from C to F
 moveLeft(); // Move from F to E
+
+
 
 console.log("Path:", moves.join(" → "));
 console.log("Total Moves (Start from 'A'):", moves.length - 1);
